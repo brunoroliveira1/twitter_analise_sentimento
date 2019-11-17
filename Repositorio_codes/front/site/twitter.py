4eee7d77037e02172f47372a0ba31858c9c10b43
lbolsoni <bolsoni.luisa@gmail.com>
2019-11-17 03:17:16 -0300

    finalizando por hoje

--- Repositorio_codes/front/site/twitter.py
+++ Repositorio_codes/front/site/twitter.py
@@ -7,11 +7,31 @@ from tweepy import Stream
 import os
 import twitter_keys
 import request
+import json
+import pandas as pd
+import numpy as np
 
+import matplotlib.pyplot as plt
+
+import preprocessor as p
+from textblob import TextBlob
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+
+import re
+import json
+import string
+from collections import Counter
+
+import nltk
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+set(stopwords.words('english'))
+from nltk.tokenize import word_tokenize
+nltk.download('punkt')
 
 global stream
 
-from flask import (Flask, request, session, g, redirect, url_for, abort, render_template, flash, Response)
+from flask import (Flask, request, session, g, redirect, url_for, abort, render_template, flash, Response,jsonify)
 
 app = Flask(__name__)
 app.secret_key = 'icc'
@@ -45,7 +65,29 @@ def criar():
     stream = Stream(auth, l)
     stream.filter(track=lista, languages=["en"])
     stream.disconnect()
-    return redirect('/pesquisa')
+        
+    
+    # Recupera o arquivo JSON com os tweets rastreados
+    tweets_data_path = 'tweets.json'
+    tweets_data = []
+
+    tweets_file = open(tweets_data_path, 'r')
+    for line in tweets_file:
+        try:
+            tweet = json.loads(line)
+            tweets_data.append(tweet)
+        except:
+            continue
+
+    # Cria um DataFrame com alguns campos importantes do JSON
+    tweets = pd.DataFrame()
+    tweets['Username'] = list(map(lambda tweet: tweet['user']['screen_name'], tweets_data))
+    tweets['Text'] = list(map(lambda tweet: tweet['text'], tweets_data))
+    tweets['Location'] = list(map(lambda tweet: tweet['user']['location'], tweets_data))
+    tweets['Timestamp'] = list(map(lambda tweet: tweet['created_at'], tweets_data))
+    
+    
+    return render_template('simple.html',  tables=[tweets.to_html(classes='data')], titles=tweets.columns.values)
 
 
 @app.route('/pesquisa')
